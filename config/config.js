daff693c37288c9de5c1f4a2b6b0e1eb6ac79659
buzo1234 <karandua2002@gmail.com>
2022-11-02 13:53:41 +0530

    set headers options

--- config/config.js
+++ config/config.js
@@ -3,39 +3,24 @@ const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
 const { SERVER_PORT } = require('../constants/constants');
-
 // Application configurations
-/* {
+const PORT = SERVER_PORT;
+/* app.use(cors()); */
+app.use(
+  cors({
     origin: [
       'http://localhost:3000',
-      'http://localhost:3001',
       'http://127.0.0.1:3000',
-      'http://127.0.0.1:3001',
       'https://www.tejdharart.com',
       'https://tejdharart.com',
     ],
-    credentials: true,
-  } */
-const PORT = SERVER_PORT;
-/* var corsOptions = {
-  origin: '*',
-  credentials: true, // some legacy browsers (IE11, various SmartTVs) choke on 204
-}; */
-/* app.use(cors()); */
+  })
+);
+app.options('*', cors());
 app.use(express.json());
-//app.use(cors());
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
-//app.options('*', cors());
+
 // Application Routing
 app.use('/', require('../routes/router'));
-
 // Database configurations
 const db = mongoose;
 module.exports = {
