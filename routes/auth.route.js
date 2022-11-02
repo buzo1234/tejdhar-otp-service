4db5e3310114e5f7e74efb162782d9cbe52181b4
buzo1234 <karandua2002@gmail.com>
2022-11-02 13:16:38 +0530

    set headers options

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,13 +1,6 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth.controller');
 
-router.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
-
 router.post('/', authController.signUpUser);
 router.post('/verify', authController.verifyEmail);
 router.post('/logout', authController.logoutUser);
@@ -18,4 +11,10 @@ router.post('/atc', authController.addToCart);
 router.post('/getorders', authController.allOrders);
 router.post('/adminorders', authController.adminOrders);
 
+router.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 module.exports = router;
