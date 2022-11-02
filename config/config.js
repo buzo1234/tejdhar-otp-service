7bf35cab651604940613700151cf70145369c932
buzo1234 <karandua2002@gmail.com>
2022-11-02 13:21:04 +0530

    set headers options

--- config/config.js
+++ config/config.js
@@ -24,6 +24,14 @@ const PORT = SERVER_PORT;
 /* app.use(cors()); */
 app.use(express.json());
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 // Application Routing
 app.use('/', require('../routes/router'));
 
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -11,10 +11,4 @@ router.post('/atc', authController.addToCart);
 router.post('/getorders', authController.allOrders);
 router.post('/adminorders', authController.adminOrders);
 
-router.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
 module.exports = router;
