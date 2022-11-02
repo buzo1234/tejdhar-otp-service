ce65727666087b6ea1e843a4e75bdecd7efa632f
buzo1234 <karandua2002@gmail.com>
2022-11-02 16:46:26 +0530

    commonJS support

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const { app, db, PORT } = require('./config/config');
 const { SERVER_DB_URI } = require('./constants/constants');
 const bootstrap = async () => {
   try {
-    await db.connect(SERVER_DB_URI);
+    await db
+      .connect(SERVER_DB_URI)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
     app.listen(PORT, async () => {
       console.log(`Server running at port ${PORT}`);
     });
