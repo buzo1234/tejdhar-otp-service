f3db9dc024d4a551c170b21678e17371e4bc767a
buzo1234 <karandua2002@gmail.com>
2022-11-05 12:15:08 +0530

    status test

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -309,7 +309,7 @@ module.exports.showOrders = async (req, res) => {
 module.exports.changeStatus = async (req, res) => {
   const { status, phone, id } = req.body;
   await User.findOne(
-    { 'orders._id': { $elemMatch: { email: phone } } },
+    { orders: { $elemMatch: { _id: id } } },
     function (err, user) {
       if (err) {
         res.send([false, err]);
