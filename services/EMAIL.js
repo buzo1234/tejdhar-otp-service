8355ebb340bf9a2cecd8045763c26ca325534144
buzo1234 <karandua2002@gmail.com>
2023-03-07 18:35:57 +0530

    all done

--- services/EMAIL.js
+++ services/EMAIL.js
@@ -8,7 +8,7 @@ module.exports.sendEmail = async (params) => {
     if (params.prod && params.prod.length > 0) {
       info = await transporter.sendMail({
         from: MAIL_SETTINGS.auth.user,
-        to: 'karandua2002@gmail.com', // list of receivers
+        to: 'tejdharart@gmail.com', // list of receivers
         subject: params.subject, // Subject line
         html: `
         <div
@@ -29,17 +29,17 @@ module.exports.sendEmail = async (params) => {
         </div>
       `,
       });
-    }else if(params.subject === 'New Order Received'){
+    } else if (params.subject === 'New Order Received') {
       info = await transporter.sendMail({
         from: MAIL_SETTINGS.auth.user,
-        to: 'karandua2002@gmail.com', // list of receivers
+        to: 'tejdharart@gmail.com', // list of receivers
         subject: params.subject, // Subject line
         html: `
         <div
           class="container"
           style="max-width: 90%; margin: auto; padding-top: 20px"
         >
-          <h2>New Custom Order Details</h2>
+          <h2>You just received a new order</h2>
           <p style="margin-bottom: 3px; font-weight:bold;">Name:</p>
           <div style="margin-bottom: 30px;">${params.name}</div>
           <p style="margin-bottom:3px; font-weight:bold;">Phone Number:</p>
@@ -50,17 +50,16 @@ module.exports.sendEmail = async (params) => {
       `,
       });
     } else {
-      
       info = await transporter.sendMail({
         from: MAIL_SETTINGS.auth.user,
-        to: 'karandua2002@gmail.com', // list of receivers
+        to: 'tejdharart@gmail.com', // list of receivers
         subject: params.subject, // Subject line
         html: `
         <div
           class="container"
           style="max-width: 90%; margin: auto; padding-top: 20px"
         >
-          <h2>New Custom Order Details</h2>
+          <h2>New Request Details</h2>
           <p style="margin-bottom: 3px; font-weight:bold;">Name:</p>
           <div style="margin-bottom: 30px;">${params.name}</div>
           <p style="margin-bottom:3px; font-weight:bold;">Phone Number:</p>
